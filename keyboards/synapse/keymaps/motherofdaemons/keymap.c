359ceb6ad359ea23a17ef5de4df841390dca2083
Lilith Screws <lilith@queercoded.io>
2022-10-28 00:27:09 -0400

    Formatting but idk if I like it

--- keyboards/synapse/keymaps/motherofdaemons/keymap.c
+++ keyboards/synapse/keymaps/motherofdaemons/keymap.c
@@ -36,10 +36,10 @@ enum combo_events {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_default(
-    KC_GRV,  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T, KC_Y, KC_U,  KC_I,    KC_O,   KC_P,    KC_QUOT,
-    KC_LCTL,  KC_A,  KC_S,  KC_D,  KC_SF, KC_G, KC_H, KC_SJ, KC_K,    KC_L,   KC_SCLN,
-    KC_LSFT, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B, KC_N, KC_M,  KC_COMM, KC_DOT, KC_SLSH,
-    _______, KC_LGUI, KC_LALT, KC_DN_BSPC,    KC_UP_SPC, KC_RALT,   KC_RGUI, KC_RCTL
+    KC_GRV,  KC_Q,    KC_W,    KC_E,          KC_R,      KC_T,      KC_Y,    KC_U,  KC_I,    KC_O,   KC_P,    KC_QUOT,
+    KC_LCTL, KC_A,    KC_S,    KC_D,          KC_SF,     KC_G,      KC_H,    KC_SJ, KC_K,    KC_L,   KC_SCLN,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,          KC_V,      KC_B,      KC_N,    KC_M,  KC_COMM, KC_DOT, KC_SLSH,
+    _______, KC_LGUI, KC_LALT, KC_DN_BSPC,    KC_UP_SPC, KC_RALT,   KC_RGUI, _______
   ),
 
  [_SYM] = LAYOUT_default(
